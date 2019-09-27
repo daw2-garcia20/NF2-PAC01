96435665f546d0463f7f49c6ba1799f1744c6f62
daw2-garcia20 <55799101+daw2-garcia20@users.noreply.github.com>
2019-09-27 17:05:15 +0200

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -39,7 +39,7 @@ if ($month ==  6) { echo '30'; }
 if ($month ==  7) { echo '31'; }
 if ($month ==  8) { echo '31'; }
 if ($month ==  9) { echo 'septiembre'; }
-if ($month == 10) { echo '31'; }
+if ($month == 10) { echo 'octubre'; }
 if ($month == 11) { echo '30'; }
 if ($month == 12) { echo '31'; }
 echo "<br>";
